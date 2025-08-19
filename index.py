e9eb3cbfbf3abeba94ce6f68a48e0e9bd8b1c7aa
Gabriel <gabrieljcoutinho@gmail.com>
2025-08-19 14:30:48 -0300

    conversor de temperatura

---
+++ index.py
@@ -0,0 +1,3 @@
+temperaturaC = float(input('Informe a temperatura em Celsius: '))
+temperaturaF = (((9*temperaturaC)/5) + 32)
+print('A temperatura de {} ºC corresponde a {} ºF'.format(temperaturaC,temperaturaF))
